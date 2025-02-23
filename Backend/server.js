a36d0c1b950ff574b44468481dddbdf9a38c5af7
Dileep <dileep@mac.home>
2025-02-23 20:59:07 +0000

    minor test with cors issue

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://nasa-snowy.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
