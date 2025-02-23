39c41442551b87242f555e7b3b8dffecdd8eef66
Dileep <dileep@mac.home>
2025-02-23 14:40:25 +0000

    minor changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
     >
       <div className="relative z-10 flex flex-col items-center">
         <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
-          Explore the Universe with NASA API
+        Unveil the Cosmos with NASA's Infinite Insights
         </h1>
         <p className="my-6 max-w-2xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
         Embark on an interstellar journey with Cosmic Explorer – a NASA API-powered web application. Dive into mesmerizing space imagery, explore celestial phenomena, and uncover detailed insights about the universe, all at your fingertips! 🚀✨
@@ -54,7 +54,7 @@ const Home = () => {
           }}
           className="group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
         >
-          Get Started
+          Embark Now
           <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
         </motion.button>
       </div>
